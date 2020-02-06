0eed66315561d0eb13455014987066ff9dd3bfd0
Julian Rex <julian.rechsteiner@gmail.com>
2020-02-06 00:07:08 -0700

    not sure if this will work

---
+++ client/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+      <nav>
+        <ul>
+          <li>Hello</li>
+          <li>Hello</li>
+        </ul>
+      </nav>
+    );
+}
+
+export default Header;
